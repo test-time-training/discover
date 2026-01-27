ca1d30c71703f631063fe19218e70e9793076a44
yudduy <duyn162005@gmail.com>
2026-01-26 22:43:56 -0800

    fix: remove group_size=1 override that disabled G normalization in PUCT
    The hardcoded `self.group_size = 1` override (marked TODO by mert) nullified
    the G normalization in the PUCT formula, causing exploration bonuses to decay
    at the raw visit-count rate instead of the intended effective-visit rate (n/G).

--- tinker_cookbook/recipes/ttt/sampler.py
+++ tinker_cookbook/recipes/ttt/sampler.py
@@ -352,8 +352,6 @@ class PUCTSampler(StateSampler):
         self.topk_children = topk_children
         self.puct_c = float(puct_c)
         self.group_size = int(group_size)
-        # TODO(mert): remove this
-        self.group_size = 1
         
         self._states: list[State] = []
         self._initial_states: list[State] = []
